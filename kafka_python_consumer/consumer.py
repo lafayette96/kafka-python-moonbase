7aefebe3e5067c4e10f162185272655823088a50
IamEld3st <iameld3st@gmail.com>
2022-08-09 20:46:02 +0200

    Change to zonechange

--- kafka_python_consumer/consumer.py
+++ kafka_python_consumer/consumer.py
@@ -1,6 +1,27 @@
 from kafka import KafkaConsumer
+import json
+import time
 
-consumer = KafkaConsumer('rover-metrics', bootstrap_servers='rover-cluster-kafka-bootstrap:9092')
+consumer = KafkaConsumer('rover-zonechange',
+                         bootstrap_servers='rover-cluster-kafka-bootstrap:9092',
+                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+last_seen = {}
 
 for msg in consumer:
-    print (msg)
+    data = msg.value
+
+    if data['previousZoneId']:
+        print(f"Rover {data['carId']} left {data['previousZoneId']} at {time.time()}")
+        last_seen[data['carId']] = {
+            'last_zone': data['previousZoneId'],
+            'timestamp': time.time()
+        }
+        
+    elif data['nextZoneId']:
+        print(f"Rover {data['carId']} entered {data['nextZoneId']} at {time.time()}")
+        if data['carId'] in last_seen:
+            print(f"  - This rover was last seen in {last_seen[data['carId']]['last_zone']} trip took {time.time()-last_seen[data['carId']]['timestamp']}")
+        
+    else:
+        print('Zone update malformed.')
