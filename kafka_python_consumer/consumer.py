6710483b038dfc75131f3c80acbc7825836ae6e3
lafayette96 <adek28000@gmail.com>
2022-08-10 13:22:46 +0200

    outposts mapped to new names

--- kafka_python_consumer/consumer.py
+++ kafka_python_consumer/consumer.py
@@ -22,22 +22,44 @@ rover_dict ={
     "a141df52-f80a-4a48-9773-cb4fdd67f8eb": "Charlie"
 }
 
+checkpoint_dict = {
+    "moon-checkpoint-a": "Checkpoint A - Dallas",
+    "moon-checkpoint-b": "Checkpoint B - Washington",
+    "moon-checkpoint-c": "Checkpoint C - New York",
+    "moon-checkpoint-d": "Checkpoint D - Raleigh",
+    "moon-checkpoint-e": "Checkpoint E - New Orleans",
+    "moon-checkpoint-f": "Checkpoint F - Las Vegas",
+    "moon-checkpoint-g": "Checkpoint G - San Francisco",
+    "moon-checkpoint-h": "Checkpoint H - Los Angeles",
+    "moon-outpost-1": "Outpost 1 - Tijuana", 
+    "moon-outpost-2": "Outpost 2 - Sydney",
+    "moon-outpost-3": "Outpost 3 - Krakow",
+    "moon-outpost-4": "Outpost 4 - Warsaw",
+    "moon-outpost-5": "Outpost 5 - Prague",
+    "moon-outpost-6": "Outpost 6 - Brno",
+    "moon-outpost-7": "Outpost 7 - London"
+}
+
 for msg in consumer:
     data = msg.value
 
     if data['previousZoneId']:
         if data['carId'] in rover_dict:
             car_name = rover_dict[data['carId']]
-        print(f"Rover {car_name} left {data['previousZoneId']} at {time.time()}")
+        if data['previousZoneId'] in checkpoint_dict:
+            zone_name = checkpoint_dict[data['previousZoneId']]
+        print(f"Rover {car_name} left {zone_name} at {time.time()}")
         last_seen[data['carId']] = {
-            'last_zone': data['previousZoneId'],
+            'last_zone': zone_name,
             'timestamp': time.time()
         }
         
     elif data['nextZoneId']:
         if data['carId'] in rover_dict:
             car_name = rover_dict[data['carId']]
-        print(f"Rover {car_name} entered {data['nextZoneId']} at {time.time()}")
+        if data['nextZoneId'] in checkpoint_dict:
+            zone_name = checkpoint_dict[data['nextZoneId']]
+        print(f"Rover {car_name} entered {zone_name} at {time.time()}")
         if data['carId'] in last_seen:
             print(f"  - This rover was last seen in {last_seen[data['carId']]['last_zone']} trip took {time.time()-last_seen[data['carId']]['timestamp']}")
         
