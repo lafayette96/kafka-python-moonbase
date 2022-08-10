36d3c273c5a33030e0e0e33634a988c1d986795f
lafayette96 <adek28000@gmail.com>
2022-08-10 10:54:20 +0200

    goood changes

--- kafka_python_consumer/consumer.py
+++ kafka_python_consumer/consumer.py
@@ -11,20 +11,19 @@ last_seen = {}
 
 for msg in consumer:
     data = msg.value
-    #data['carId'] = "Alpha"
     names_list=["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett"]
 
     if data['previousZoneId']:
         print(f"Rover {random.choice(names_list)} left {data['previousZoneId']} at {time.time()}")
-        last_seen[random.choice(names_list)] = {
+        last_seen[data['carId']] = {
             'last_zone': data['previousZoneId'],
             'timestamp': time.time()
         }
         
     elif data['nextZoneId']:
-        print(f"Rover {random.choice(names_list)} entered {data['nextZoneId']} at {time.time()}")
+        print(f"Rover {data['carId']} entered {data['nextZoneId']} at {time.time()}")
         if data['carId'] in last_seen:
-            print(f"  - This rover was last seen in {last_seen[random.choice(names_list)]['last_zone']} trip took {time.time()-last_seen[data['carId']]['timestamp']}")
+            print(f"  - This rover was last seen in {last_seen[data['carId']]['last_zone']} trip took {time.time()-last_seen[data['carId']]['timestamp']}")
         
     else:
         print('Zone update malformed.')
